bb0dff4713a07b916263210dfc487789c7f2314e
agomila96@gmail.com <aitor2008>
2020-12-08 02:41:51 +0100

    Fix

--- lib/src/views/ui/home.dart
+++ lib/src/views/ui/home.dart
@@ -5,7 +5,15 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: customAppBar,
-    );
+        appBar: customAppBar,
+        body: Flex(
+          direction: Axis.horizontal,
+          children: [
+            Column(
+              children: [Text("")],
+            ),
+            Column(children: [Text("")])
+          ],
+        ));
   }
 }

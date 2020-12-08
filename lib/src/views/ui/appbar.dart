9ec920ccd363860d6f42f5884427506a59dd7431
agomila96@gmail.com <aitor2008>
2020-12-08 14:34:21 +0100

    Add settings icon

--- lib/src/views/ui/appbar.dart
+++ lib/src/views/ui/appbar.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
-var customAppBar = AppBar(title: Text("XStore Fetch Test"));
+var customAppBar = AppBar(
+  title: Text("XStore Fetch Test"),
+  actions: [
+    IconButton(
+      onPressed: () {},
+      icon: Icon(Icons.settings),
+    )
+  ],
+);

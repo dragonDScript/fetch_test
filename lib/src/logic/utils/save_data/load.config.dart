2e4c1bb4747da73e01a1a9a42af0f4954f80b92a
agomila96@gmail.com <aitor2008>
2020-12-10 22:34:29 +0100

    Fix load function

--- lib/src/logic/utils/save_data/load.config.dart
+++ lib/src/logic/utils/save_data/load.config.dart
@@ -2,9 +2,8 @@ import 'dart:io' as io;
 import 'package:path_provider/path_provider.dart' as p;
 import 'package:path/path.dart' as p;
 import 'dart:convert' as convert;
-import 'dart:async';
 
-Future<String> load() {
+Future<Map<String, dynamic>> load() async {
   Future<io.Directory> docPath = p.getApplicationDocumentsDirectory();
   String path;
   docPath.then((res) {
@@ -12,5 +11,7 @@ Future<String> load() {
   });
 
   io.File read = io.File(path.toString());
-  return read.readAsString();
+  var readedFile = await read.readAsString();
+  Map<String, dynamic> object = convert.jsonDecode(readedFile);
+  return object;
 }

6023d13a838be07ebf702e0c9c4287dbf94548c8
agomila96@gmail.com <aitor2008>
2020-12-13 20:45:31 +0100

    Use list for the config

--- lib/src/logic/utils/save_data/load.config.dart
+++ lib/src/logic/utils/save_data/load.config.dart
@@ -3,12 +3,12 @@ import 'package:path_provider/path_provider.dart' as p_provider;
 import 'package:path/path.dart' as p;
 import 'dart:convert' as convert;
 
-Future<Map<String, dynamic>> load() async {
+Future<List<Map<String, dynamic>>> load() async {
   Future<io.Directory> docPath = p_provider.getApplicationDocumentsDirectory();
   String path = p.join(docPath.toString(), '.xstoreconfig.json');
 
   io.File read = io.File(path.toString());
   var readedFile = await read.readAsString();
-  Map<String, dynamic> object = convert.jsonDecode(readedFile);
+  List<Map<String, dynamic>> object = convert.jsonDecode(readedFile);
   return object;
 }

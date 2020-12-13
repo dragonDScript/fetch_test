4950b343eefb87732a9183fef77a450a9835be4f
agomila96@gmail.com <aitor2008>
2020-12-13 20:52:56 +0100

    Make abstraction in save.config.dart

--- lib/src/logic/utils/save_data/save.config.dart
+++ lib/src/logic/utils/save_data/save.config.dart
@@ -4,12 +4,10 @@ import 'package:path/path.dart' as p;
 import 'dart:convert' as convert;
 import 'dart:async';
 
-void save(String name, dynamic value) {
-  // convert fields to json
-  String json = convert.jsonEncode({name: name, value: value});
+void save(String content) {
   Future<io.Directory> docPath = p_provider.getApplicationDocumentsDirectory();
   String path = p.join(docPath.toString(), '.xstoreconfig.json');
 
   io.File write = io.File(path.toString());
-  write.writeAsString(json); // write json contents
+  write.writeAsString(content); // write json contents
 }

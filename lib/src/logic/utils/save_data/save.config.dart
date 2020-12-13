05fb5138d00891bfde9197cb9190d781320bb065
agomila96@gmail.com <aitor2008>
2020-12-13 20:10:13 +0100

    Fix another bug with imports

--- lib/src/logic/utils/save_data/save.config.dart
+++ lib/src/logic/utils/save_data/save.config.dart
@@ -1,5 +1,5 @@
 import 'dart:io' as io;
-import 'package:path_provider/path_provider.dart' as p;
+import 'package:path_provider/path_provider.dart' as p_provider;
 import 'package:path/path.dart' as p;
 import 'dart:convert' as convert;
 import 'dart:async';
@@ -7,7 +7,7 @@ import 'dart:async';
 void save(String name, dynamic value) {
   // convert fields to json
   String json = convert.jsonEncode({name: name, value: value});
-  Future<io.Directory> docPath = p.getApplicationDocumentsDirectory();
+  Future<io.Directory> docPath = p_provider.getApplicationDocumentsDirectory();
   String path = p.join(docPath.toString(), '.xstoreconfig.json');
 
   io.File write = io.File(path.toString());

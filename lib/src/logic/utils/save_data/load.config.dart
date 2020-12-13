4141be86699b1a937f245aa435325cd5e390686a
agomila96@gmail.com <aitor2008>
2020-12-13 20:09:50 +0100

    Fix bug with imports

--- lib/src/logic/utils/save_data/load.config.dart
+++ lib/src/logic/utils/save_data/load.config.dart
@@ -1,10 +1,10 @@
 import 'dart:io' as io;
-import 'package:path_provider/path_provider.dart' as p;
+import 'package:path_provider/path_provider.dart' as p_provider;
 import 'package:path/path.dart' as p;
 import 'dart:convert' as convert;
 
 Future<Map<String, dynamic>> load() async {
-  Future<io.Directory> docPath = p.getApplicationDocumentsDirectory();
+  Future<io.Directory> docPath = p_provider.getApplicationDocumentsDirectory();
   String path = p.join(docPath.toString(), '.xstoreconfig.json');
 
   io.File read = io.File(path.toString());

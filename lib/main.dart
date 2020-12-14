90b2bca1e1e69de17fb021b699f66ae538d9091f
agomila96@gmail.com <aitor2008>
2020-12-14 22:07:16 +0100

    Create configuration file if it is missing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fetch_test/src/app.dart';
+import 'package:fetch_test/src/logic/utils/fs/create_config_file.dart' as ccf;
 
 void main() {
+  ccf.createConfigFile();
   runApp(MyApp());
 }
---
+++ lib/src/logic/utils/fs/create_config_file.dart
@@ -0,0 +1,14 @@
+import 'package:fetch_test/src/logic/utils/save_data/save.config.dart' as save;
+import 'dart:io' as io;
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart' as p_provider;
+
+void createConfigFile() async {
+  //Create configuration file if it is missing
+  io.Directory docPath = await p_provider.getApplicationDocumentsDirectory();
+  String path = p.join(docPath.path, '.xstoreconfig.json');
+
+  if (!io.File(path).existsSync()) {
+    save.save('[]');
+  }
+}

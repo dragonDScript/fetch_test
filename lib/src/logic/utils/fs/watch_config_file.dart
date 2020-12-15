cca0825f5aeaf1f67132ac58d7baf8088132e867
agomila96@gmail.com <aitor2008>
2020-12-15 18:28:50 +0100

    Add watcher

--- lib/src/logic/utils/fs/watch_config_file.dart
+++ lib/src/logic/utils/fs/watch_config_file.dart
@@ -3,10 +3,10 @@ import 'dart:async';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart' as p_provider;
 
-Future<Stream<FileSystemEvent>> watchConfigFile() async {
+Future<Stream<dynamic>> watchConfigFile() async {
   //get the config file
   Directory docPath = await p_provider.getApplicationDocumentsDirectory();
   String path = p.join(docPath.path, '.xstoreconfig.json');
-  //watch it (TO DO)
+  //watch it
   return new File(path).watch();
 }

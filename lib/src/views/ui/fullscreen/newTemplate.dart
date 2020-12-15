c7cd200f824b9b2a9cef3c7657fc4d21788072a7
agomila96@gmail.com <aitor2008>
2020-12-15 20:33:41 +0100

    Fix new template button

--- lib/src/views/ui/fullscreen/newTemplate.dart
+++ lib/src/views/ui/fullscreen/newTemplate.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:fetch_test/src/logic/internal/template/new.template.dart'
-    as newTemplate;
+import 'package:fetch_test/src/logic/utils/save_data/set.config.dart'
+    as setConfig;
+import 'package:fetch_test/src/logic/utils/save_data/get.config.dart'
+    as getConfig;
 
 class NewTemplate extends StatefulWidget {
   @override
@@ -23,8 +25,9 @@ class _NewTemplate extends State<StatefulWidget> {
           actions: [
             IconButton(
               icon: Icon(Icons.check),
-              onPressed: () {
-                newTemplate.newTemplate({
+              onPressed: () async {
+                List<dynamic> config = await getConfig.getConfig('templates');
+                config.add({
                   'title': this.title,
                   'description': this.description,
                   'icon': this.icon,
@@ -32,6 +35,8 @@ class _NewTemplate extends State<StatefulWidget> {
                   'headers': this.headers,
                   'isFavourite': this.isFavourite
                 });
+                setConfig.setConfig('templates', config);
+
                 Navigator.pop(context);
               },
               tooltip: "Save",

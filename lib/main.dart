99e595078c76fdcbe0e4e65ed09daed26a60506e
agomila96@gmail.com <aitor2008>
2020-12-17 21:50:38 +0100

    Ensure initzialitzation before running

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:fetch_test/src/app.dart';
 import 'package:fetch_test/src/logic/utils/fs/create_config_file.dart' as ccf;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   ccf.createConfigFile();
   runApp(MyApp());
 }

23484a782cad3d1410755a221fcadd190e585132
agomila96@gmail.com <aitor2008>
2020-12-18 19:15:42 +0100

    Use streams instead of futures

--- lib/src/views/ui/views/home.dart
+++ lib/src/views/ui/views/home.dart
@@ -10,6 +10,7 @@ import 'package:fetch_test/src/logic/utils/save_data/set.config.dart'
     as setConfig;
 import 'package:fetch_test/src/logic/utils/save_data/delete.config.dart';
 import 'dart:async';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   @override
@@ -39,56 +40,63 @@ class _Home extends State<StatefulWidget> {
                     stream: _snapshot.data,
                     builder: (BuildContext context,
                         AsyncSnapshot<dynamic> snapshot) {
-                      var list = snapshot.data['templates'];
-                      return Row(children: [
-                        Flexible(
-                            child: ListView(
-                              children: [
-                                Container(
-                                  color: Colors.grey[200],
-                                  child: TextButton.icon(
-                                      icon: Icon(Icons.add),
-                                      label: Text("Create new template"),
-                                      onPressed: () {
-                                        newTemplate(context);
-                                      }),
-                                ),
-                                ListView.builder(
-                                    padding: const EdgeInsets.all(8),
-                                    itemCount: list.length,
-                                    shrinkWrap: true,
-                                    itemBuilder:
-                                        (BuildContext context, int index) {
-                                      String icon = list[index]['icon'];
-                                      return TextButton.icon(
+                      if (!snapshot.hasData) {
+                        return Center(child: CircularProgressIndicator());
+                      } else {
+                        print(jsonEncode(snapshot.data));
+                        print(snapshot.hasData);
+                        var list = jsonDecode(snapshot.data)['templates'];
+                        return Row(children: [
+                          Flexible(
+                              child: ListView(
+                                children: [
+                                  Container(
+                                    color: Colors.grey[200],
+                                    child: TextButton.icon(
+                                        icon: Icon(Icons.add),
+                                        label: Text("Create new template"),
                                         onPressed: () {
-                                          //select this card when you click on it
-                                          this.selectedButton = index;
-                                        },
-                                        icon: Icon(Icons.web),
-                                        label: Text('${list[index]['title']}'),
-                                        onLongPress: () async {
-                                          deleteConfig('templates', index);
-                                        },
-                                      );
-                                    })
-                              ],
-                            ),
-                            flex: 3),
-                        Flexible(
-                            child: Column(children: [
-                              list.length > 0
-                                  ? TextFormField(
-                                      initialValue: list[selectedButton]
-                                          ['title'])
-                                  : Center(
-                                      child: Column(children: [
-                                      Icon(Icons.all_inbox),
-                                      Text("Nothing to preview")
-                                    ]))
-                            ]),
-                            flex: isDesktop == true ? 5 : null)
-                      ]);
+                                          newTemplate(context);
+                                        }),
+                                  ),
+                                  ListView.builder(
+                                      padding: const EdgeInsets.all(8),
+                                      itemCount: list.length,
+                                      shrinkWrap: true,
+                                      itemBuilder:
+                                          (BuildContext context, int index) {
+                                        String icon = list[index]['icon'];
+                                        return TextButton.icon(
+                                          onPressed: () {
+                                            //select this card when you click on it
+                                            this.selectedButton = index;
+                                          },
+                                          icon: Icon(Icons.web),
+                                          label:
+                                              Text('${list[index]['title']}'),
+                                          onLongPress: () async {
+                                            deleteConfig('templates', index);
+                                          },
+                                        );
+                                      })
+                                ],
+                              ),
+                              flex: 3),
+                          Flexible(
+                              child: Column(children: [
+                                list.length > 0
+                                    ? TextFormField(
+                                        initialValue: list[selectedButton]
+                                            ['title'])
+                                    : Center(
+                                        child: Column(children: [
+                                        Icon(Icons.all_inbox),
+                                        Text("Nothing to preview")
+                                      ]))
+                              ]),
+                              flex: isDesktop == true ? 5 : null)
+                        ]);
+                      }
                     })));
   }
 }
